79f11360f9c41bac076074643a0ca24b6c54404e
DevWurm <devwurm@devwurm.net>
2017-04-17 14:26:03 +0200

    Adding a functional test fixture

---
+++ functional_tests/fixture_1/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const { WebpackWarPlugin } = require('../../dist');
+
+module.exports = {
+  entry: {
+    file1: './src/file1.js',
+    file2: './src/file2.js',
+    file3: './src/file3.js',
+    'dir1/file4': './src/dir1/file4.js',
+  },
+  context: __dirname,
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].js"
+  },
+  plugins: [
+    new WebpackWarPlugin()
+  ]
+};
